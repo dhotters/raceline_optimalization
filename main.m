6d5adbd919332b1cefc3456e8baf2c8d610383d6
d.e.s.hotters@student.tudelft.nl <d.e.s.hotters@student.tudelft.nl>
2023-06-02 00:07:35 +0200

    TODOs

--- main.m
+++ main.m
@@ -13,3 +13,9 @@ raceline = getRaceLine(a_vec, track);
 % plot the current raceline based on the given coefficients
 hold on
 plot(raceline.x, raceline.y, 'r');
+
+%% TODO
+%% 1 - Get a velocity profile with a given raceline curve
+%% With this velocity profile compute the lap time
+%% 2 - Close the track curves
+%% 3 - Optimize the a_vec and create equality and inequality constraints
