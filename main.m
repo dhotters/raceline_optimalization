1a51c257735a42b0addb207711235657744bf2b3
Dani Hotters <d.e.s.hotters@student.tudelft.nl>
2023-06-11 19:45:30 +0200

    normalization and convergence

--- main.m
+++ main.m
@@ -2,27 +2,20 @@ clc
 clear all
 close all
 
-n = 100;
+% globals
 global track
+global car
+global raceline0
+global t0
+
+n = 400;
 track = trackReader("track\Spa.csv", n);
-
-% Vector of percentage trackwidth location of the race line
-% x0 being the initial state of the coefficients (ie the design variables)
-x0 = ones(n, 1).*0.5; 
-
-% Compute raceline with the coeffcient vector
-%raceline = getRaceLine(x0, track);
-%raceline = load('raceline100.mat').raceline;
-
-% Plot the current state
-%track_plotter(track, raceline);
 
 % Setup car
 % NOTE not a good idea to take F1 car values here cause those benefit alot
 % from aerodynamic performance hence we assume our vehicle does not benefit
 % from aerodynamics -> else the max traction etc is not correct anymore as
 % it is speed dependant
-global car
 car.mass = 750; % kg
 car.max_g_accel = 0.5; % Max G force in acceleration
 car.max_g_brake = 1; % Max G force when braking
@@ -31,8 +24,19 @@ car.cd = 0.25; % drag coefficient (assumed constant)
 car.rho = 1.225; % air density around the track
 car.S = 1.3; % frontal area m2
 
-% Test lap time
-%getLapTime(track, raceline, car)
+% Vector of percentage trackwidth location of the race line
+% x0 being the initial state of the coefficients (ie the design variables)
+x0 = ones(n, 1).*0.5; 
+
+% Compute raceline with the coeffcient vector
+raceline0 = getRaceLine(x0, track); % We use the initial raceline to normalize
+%raceline = load('raceline100.mat').raceline;
+
+% Compute lap time, used for normalization
+t0 = getLapTime(track, raceline0, car);
+
+% Plot the current state
+%track_plotter(track, raceline);
 
 %% TODO
 %% 1 - Get a velocity profile with a given raceline curve
@@ -47,8 +51,8 @@ options.FunValCheck     = 'off';
 options.PlotFcns        = {@optimplotfval, @optimplotx, @optimplotfirstorderopt, @optimplotstepsize, @optimplotconstrviolation, @optimplotfunccount};
 options.MaxIter         = 1000;
 options.MaxFunEvals     = 1e9;
-options.TolFun = 1e-9;
-options.TolX = 1e-10;
+options.TolFun = 1e-3;
+options.TolX = 1e-15;
 
 %% fmincon to try it out on curvature
 tic
@@ -65,13 +69,19 @@ track_plotter(track, getRaceLine(x, track));
 function [f] = opt(x)
     global track
     global car
+    global raceline0
+    global t0
 
     % calculate raceline given a coef vector x
     raceline = getRaceLine(x, track);
 
+    t = getLapTime(track, raceline, car) / t0; % estimate lap time
+    K = sum(raceline.rad_per_meter) / sum(raceline0.rad_per_meter); % measure of total track curvature
+    L = sum(raceline.L) / sum(raceline0.L); % path length
+
     % objective function
     %f = sum(raceline.rad_per_meter);
-    f = getLapTime(track, raceline, car);
+    f = (3*K+t)/4;
 end
 
 function [c, ceq] = constraints(x)
